0177c47aef888bf06e57f907ba78c445a6b82654
ArnePalmbergNTI2C <150041090+ArnePalmbergNTI2C@users.noreply.github.com>
2024-04-19 11:39:44 +0200

    Create game.rb

---
+++ programmering/game.rb
@@ -0,0 +1,119 @@
+def display_pinnar(pinnar)
+    output = ""
+    i = 0
+
+    while i < pinnar
+        output << "| "
+
+        i +=1
+    end
+    puts "antal pinnar kvar:"
+    puts output
+end
+
+def play(player1,player2,pinnar,vems_tur)
+
+
+    display_pinnar(pinnar)
+
+    while pinnar > 0 
+        system("cls")
+        display_pinnar(pinnar)
+
+
+        if vems_tur % 2 == 0
+            puts "#{player2} välj hur många pinnar som ska tas bort"
+            antal = gets.chomp.to_i
+            valid = false
+            while valid == false
+                valid = true
+                if antal > 3 || antal < 1
+                    valid = false
+                    puts "välj igen"
+                    antal = gets.chomp.to_i
+                elsif antal.class != Integer
+                    valid = false
+                    puts "välj igen"
+                    antal = gets.chomp
+                end
+                
+            end
+
+            pinnar -= antal
+            display_pinnar(pinnar)
+
+
+        else
+            puts "#{player1}. välj hur många pinnar som ska tas bort"
+            antal = gets.chomp.to_i
+            valid = false
+            while valid == false
+                valid = true
+                if antal > 3 || antal < 1
+                    valid = false
+                    puts "välj igen"
+                    antal = gets.chomp.to_i
+                elsif antal.class != Integer
+                    valid = false
+                    puts "välj igen"
+                    antal = gets.chomp
+                end
+                
+            end
+
+            pinnar -= antal
+            display_pinnar(pinnar)
+
+
+        end
+        
+        vems_tur += 1
+
+    end
+    return vems_tur 
+
+end
+
+def game_over(winner,player1,player2)
+    system("cls")
+
+    if winner % 2 == 0
+        puts "#{player2} vann"
+    else
+        puts "#{player1} vann"
+    end
+
+end
+
+def game()
+    
+    player1,player2,pinnar,vems_tur = set_up()
+
+    winner = play(player1,player2,pinnar,vems_tur)
+
+    game_over(winner,player1,player2)
+
+
+end
+
+def set_up()
+    puts "spelare 1 namn:"
+    player1 = gets.chomp
+    puts "spelare 1 = #{player1}"
+
+    puts "spelare 2 namn:"
+    player2 = gets.chomp
+    puts "spelare 2 = #{player2}"
+
+
+    pinnar = rand(11..16)
+
+    vems_tur = rand(1..2)
+
+    system("cls")
+
+    return player1,player2,pinnar,vems_tur
+end
+
+
+game()
